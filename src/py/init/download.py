ad1c0f4ba5d1c922f4d2c1f18cd8cd2c4b15cd1d
hjw <huandrong@163.com>
2025-08-06 10:45:05 +0800

    add download

--- src/py/init/download.py
+++ src/py/init/download.py
@@ -1,6 +1,101 @@
-from src.py.utils.log_util import default_logger as logger
+"""
+下载圣经
+http://audio2.abiblica.org/bibles/app/audio/4_1.zip
+...
+http://audio2.abiblica.org/bibles/app/audio/4_66.zip
+"""
 
+
+import pathlib
+from src.py.utils.log_util import default_logger as logger
+from src.py.utils.constants import CONFIG_FILE, DATA_DIR
+from src.py.utils.file_util import get_file_lines,save_config,load_config,check_zip_file
+
+
+def download_single_file(url:str,download_filepath:pathlib.Path):
+    """
+    下载单个文件
+    """
+    logger.info(f"开始下载文件: {download_filepath}")
+    download_filepath.parent.mkdir(parents=True, exist_ok=True)
+    logger.info(f"下载文件完成: {download_filepath}")
+    return check_zip_file(download_filepath)
+
+def download_audios():
+    """
+    下载文件
+    """
+    logger.info(f"开始下载音频文件")
+    config = load_config()
+    for audio in config["bible"]["audio_list"]:
+        if not audio["download_status"]:
+            logger.info(f"文件未下载: {audio['download_filepath']}")
+            if download_single_file(audio["url"],audio["download_filepath"]):
+                audio["download_status"] = True
+            else:
+                audio["download_status"] = False
+    save_config(config)
+    logger.info(f"下载音频文件完成")
+
+def check_download_status():
+    """
+    检查文件是否存在
+    """
+    logger.info("开始检查下载文件是否完整")
+    config = load_config()
+    for audio in config["bible"]["audio_list"]:
+        if check_zip_file(audio["download_filepath"]):
+            logger.info(f"文件完整: {audio['download_filepath']}")
+            audio["download_status"] = True
+        else:
+            logger.info(f"文件不完整: {audio['download_filepath']}")
+            audio["download_status"] = False
+    save_config(config)
+    logger.info("检查下载文件是否完整完成")
+
+def generate_config():
+    logger.info("开始生成配置文件")
+    download_dir = DATA_DIR / "audio" / "download"
+    config = {
+        "bible": {
+            "audio_list": [
+                {
+                    "url": "http://audio2.abiblica.org/bibles/app/audio/4_1.zip",
+                    "output_dir": download_dir / "4_1",
+                    "download_filepath": download_dir / "4_1.zip",
+                    "bookname": "创世记",
+                    "order": 1,
+                    "category": "旧约",
+                    "download_status": False,
+                }
+            ]
+        }
+    }
+    (download_dir / "4_1").mkdir(parents=True, exist_ok=True)
+    category_split_index = 40
+    book_name_list = get_file_lines(DATA_DIR / "text" / "book_name_list.txt")
+    for i in range(2, 67):
+        if i < category_split_index:
+            category = "旧约"
+        else:
+            category = "新约"
+
+        config["bible"]["audio_list"].append(
+            {
+                "url": f"http://audio2.abiblica.org/bibles/app/audio/4_{i}.zip",
+                "output_dir": download_dir / f"4_{i}",
+                "download_filepath": download_dir / f"4_{i}.zip",
+                "bookname": book_name_list[i - 1].strip(),
+                "order": i,
+                "category": category,
+                "download_status": False,
+            }
+        )
+        logger.info(f"开始创建目录: {download_dir/f'4_{i}'}")
+        (download_dir / f"4_{i}").mkdir(parents=True, exist_ok=True)
+    save_config(config)
+    logger.info("配置文件生成完成")
 
 
 if __name__ == "__main__":
-    logger.info("Hello, World!")
+    generate_config()
---
+++ src/py/utils/file_util.py
@@ -0,0 +1,66 @@
+"""
+文件操作工具类
+"""
+
+import os
+import json
+import pathlib
+import zipfile
+from src.py.utils.constants import CONFIG_FILE
+from src.py.utils.log_util import default_logger as logger
+
+def load_config(config_file=CONFIG_FILE):
+    logger.info(f"开始加载配置文件: {config_file}")
+    with open(config_file, "r", encoding="utf-8") as f:
+        return json.load(f)
+
+def save_config(config, config_file=CONFIG_FILE):
+    logger.info(f"开始保存配置文件: {config_file}")
+    with open(config_file, "w", encoding="utf-8") as f:
+        json.dump(config, f, indent=4, ensure_ascii=False,cls=PathEncoder)
+
+def read_file(file_path):
+    logger.info(f"开始读取文件: {file_path}")
+    with open(file_path, "r", encoding="utf-8") as f:
+        return f.read()
+
+def write_file(file_path, content):
+    logger.info(f"开始写入文件: {file_path}")
+    with open(file_path, "w", encoding="utf-8") as f:
+        f.write(content)
+
+
+def get_file_lines(file_path):
+    logger.info(f"开始获取文件内容: {file_path}")
+    with open(file_path, "r", encoding="utf-8") as f:
+        line_list = f.readlines()
+    logger.info(f"文件内容获取完成: {file_path},行数: {len(line_list)}")
+    return line_list
+
+class PathEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, pathlib.Path):
+            return str(obj)
+        return super().default(obj)
+
+
+def check_zip_file(file_path):
+    """
+    检查文件是否完整zip包
+    """
+    logger.info(f"开始检查文件是否完整zip包: {file_path}")
+    if not isinstance(file_path, pathlib.Path):
+        file_path = pathlib.Path(file_path)
+    if not file_path.exists():
+        logger.info(f"文件不存在: {file_path}")
+        return False
+    try:
+        with zipfile.ZipFile(file_path, "r") as zip_ref:
+            if len(zip_ref.namelist()) == 0:
+                logger.info(f"文件为空: {file_path}")
+                return False
+            logger.info(f"文件完整: {file_path}")
+            return True
+    except zipfile.BadZipFile:
+        logger.info(f"文件格式错误: {file_path}")
+        return False
